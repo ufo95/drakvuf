554b588f693a9a9695a8a62718485f028d0fad0e
uli <fourier@in.tum.de>
2018-07-21 17:07:51 +0200

    every compilation commit until now

---
+++ travis-scripts/prepare_chroot_dir.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Based on a test script from avsm/ocaml repo https://github.com/avsm/ocaml
+
+CHROOT_DIR=/srv/arm-chroot
+TRAVIS_BUILD_DIR=/home/ufo/git_stuff/drakvuf
+
+sudo tar -xf ${TRAVIS_BUILD_DIR}/test-packages/arm-chroot.tar.gz -C /srv/
+sudo cp /etc/resolv.conf ${CHROOT_DIR}/etc/resolv.conf
+sudo mkdir -p ${CHROOT_DIR}/${TRAVIS_BUILD_DIR}
+sudo rsync -a ${TRAVIS_BUILD_DIR}/ ${CHROOT_DIR}/${TRAVIS_BUILD_DIR}/
+
+# Call ourselves again which will cause tests to run
+sudo chroot ${CHROOT_DIR} bash -c "cd ${TRAVIS_BUILD_DIR} && ./travis-scripts/inside_chroot.sh"
