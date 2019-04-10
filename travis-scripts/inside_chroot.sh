d23f22133d4301575928c4e072cda93b7cfac7f4
Ulrich Fourier <ufo95@gmx.de>
2019-04-10 18:42:23 +0200

    initial test stuff

---
+++ travis-scripts/inside_chroot.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+apt update
+apt install -y make autoconf autoconf-archive git libtool pkg-config libglib2.0-dev bison flex libjson-c-dev clang
+# for xen-arm:
+#apt install -y python-dev gettext iasl uuid-dev libpixman-1-dev ftp libyajl-dev libfdt-dev
+
+echo $PWD
+#install prebuild xen
+dpkg -i test-packages/xen_1-1_arm64.deb
+
+#build & install libvmi
+#git submodule update --init libvmi
+#cd libvmi/
+#echo $PWD
+cd ..
+git clone https://github.com/libvmi/libvmi.git
+cd libvmi
+
+./autogen.sh
+./configure --disable-kvm --disable-examples
+make -j
+make install
+
+cd ../drakvuf
+#build drakvuf
+#export CXX="clang++-3.8"
+autoreconf -vi
+./configure --disable-plugin-poolmon --disable-plugin-filetracer --disable-plugin-filedelete --disable-plugin-objmon --disable-plugin-exmon --disable-plugin-ssdtmon --disable-plugin-debugmon --disable-plugin-cpuidmon --disable-plugin-socketmon --disable-plugin-regmon --disable-plugin-procmon
+make -j
