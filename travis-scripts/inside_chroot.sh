9b262b21713034087a7b2587b2ef8da2b455a02d
Ulrich Fourier <ufo95@gmx.de>
2019-05-04 15:34:56 +0200

    works locally

--- travis-scripts/inside_chroot.sh
+++ travis-scripts/inside_chroot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt update
-apt install -y make autoconf autoconf-archive git libtool pkg-config libglib2.0-dev bison flex libjson-c-dev clang cmake check
+apt install -y make autoconf autoconf-archive git libtool pkg-config libglib2.0-dev bison flex libjson-c-dev clang cmake check libvirt-dev
 # for xen-arm:
 #apt install -y python-dev gettext iasl uuid-dev libpixman-1-dev ftp libyajl-dev libfdt-dev
 
@@ -18,12 +18,13 @@ cd libvmi
 
 mkdir build
 cd build
-cmake -DENABLE_KVM=OFF -DENABLE_FILE=OFF ..
-#install??
+cmake -DENABLE_BAREFLANK=OFF -DENABLE_KVM=off ..
+make -j3
+make install
 
-cd ../drakvuf
+cd ../../
 #build drakvuf
 #export CXX="clang++-3.8"
 autoreconf -vi
-./configure --disable-plugin-poolmon --disable-plugin-filetracer --disable-plugin-filedelete --disable-plugin-objmon --disable-plugin-exmon --disable-plugin-ssdtmon --disable-plugin-debugmon --disable-plugin-cpuidmon --disable-plugin-socketmon --disable-plugin-regmon --disable-plugin-procmon
+./configure --disable-plugin-poolmon --disable-plugin-filetracer --disable-plugin-filedelete --disable-plugin-objmon --disable-plugin-exmon --disable-plugin-ssdtmon --disable-plugin-debugmon --disable-plugin-cpuidmon --disable-plugin-socketmon --disable-plugin-regmon --disable-plugin-procmon --disable-plugin-bsodmon --disable-plugin-envmon --disable-plugin-crashmon --disable-plugin-clipboardmon --disable-plugin-windowmon --disable-plugin-librarymon --disable-plugin-dkommon
 make -j
